11bc40c39f3bab7c66f54af9885f523d9f487e96
feijeff0486 <feijeff0486@gmail.com>
2020-07-08 10:20:26 +0800

    :tada:提交示例demo

---
+++ app/src/main/java/com/jeff/action/apkbuild/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jeff.action.apkbuild;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
